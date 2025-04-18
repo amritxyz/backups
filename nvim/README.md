8defdf8d8eab04a65d4c17115fb769721bde0707
amritxyz <amrit44404@proton.me>
2025-04-18 17:58:39 +0545

    backup for neovim with solarized-theme

---
+++ nvim/README.md
@@ -0,0 +1,17 @@
+# Kickstart-Nvim
+
+A simple, minimal, modular neovim config inspired by [kickstart-nvim](https://github.com/nvim-lua/kickstart.nvim).
+
+---
+
+## Installation
+
+```bash
+git clone --depth=1 https://github.com/amritxyz/kickstart-nvim.git ~/.config/nvim
+nvim
+```
+
+---
+
+## Plugins (Via. Lazy.nvim)
+[**All-Plugins**](https://github.com/amritxyz/kickstart-nvim/blob/main/lua/plugins).
---
+++ nvim/init.lua
@@ -0,0 +1,58 @@
+require 'config.set'
+require 'config.keymap'
+require 'config.undercurl'
+require 'config.api'
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  'tpope/vim-sleuth',
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+  -- require 'kickstart.plugins.debug',
+  -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  { import = 'plugins' }, -- [ lua/plugins ]
+}, {
+  ui = {
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
