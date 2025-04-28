d36224b7e9f1bae16a7e8fbe6e5ae50e2dd8949a
amritxyz <amrit44404@proton.me>
2025-04-28 22:15:33 +0545

    just some awesome colorPallettes

---
+++ nvim/color-gruvbox_material.lua
@@ -0,0 +1,8 @@
+return {
+	"sainnhe/gruvbox-material",
+	priority = 1000,
+	init = function()
+		vim.cmd.colorscheme("gruvbox-material")
+		vim.cmd.hi("Comment gui=none")
+	end,
+}
---
+++ nvim/color-solarized_monochrome.lua
@@ -0,0 +1,8 @@
+return {
+	"idr4n/github-monochrome.nvim",
+	priority = 1000,
+	config = function()
+		require("github-monochrome").setup({})
+		vim.cmd.colorscheme("github-monochrome-solarized")
+	end,
+}
